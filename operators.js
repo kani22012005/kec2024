1ef4196fe1341367bf8310d41c8933ec60d3ac65
kani22012005 <kanishkr.22aim@kongu.edu>
2024-12-28 14:30:18 +0530

    second commit

---
+++ operators.js
@@ -0,0 +1,24 @@
+// Assignment Operator
+var a = 10;
+console.log(a);
+
+// Unary Operator
+a++;
+console.log(a);
+
+// Arithmetic Operators
+var var1 = 10;
+var var2 = 30;
+var sum = var1 + var2;
+console.log("Sum:", sum);
+
+// Relational Operators
+var rel1 = 15;
+var rel2 = 20;
+
+console.log("rel1 > rel2:", rel1 > rel2);
+console.log("rel1 < rel2:", rel1 < rel2);
+console.log("rel1 >= rel2:", rel1 >= rel2);
+console.log("rel1 <= rel2:", rel1 <= rel2);
+console.log("rel1 == rel2:", rel1 == rel2);
+console.log("rel1 != rel2:", rel1 != rel2);
